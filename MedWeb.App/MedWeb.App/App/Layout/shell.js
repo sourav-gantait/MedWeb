b263f8bb36e860be2c22b8a4f49fa85b9b2ed289
sourav-gantait <sourav_gantait@yahoo.co.in>
2016-01-17 19:19:34 +0530

    Base version upload

---
+++ MedWeb.App/MedWeb.App/App/Layout/shell.js
@@ -0,0 +1,59 @@
+﻿(function () { 
+    'use strict';
+    
+    var controllerId = 'shell';
+    angular.module('app').controller(controllerId,
+        ['$rootScope', '$location','common', 'config', 'authService', shell]);
+
+    function shell($rootScope,$location, common, config, authService) {
+        var vm = this;
+        var logSuccess = common.logger.getLogFn(controllerId, 'success');
+        var events = config.events;
+        vm.busyMessage = 'Please wait ...';
+        vm.isBusy = true;
+        vm.spinnerOptions = {
+            radius: 40,
+            lines: 7,
+            length: 0,
+            width: 30,
+            speed: 1.7,
+            corners: 1.0,
+            trail: 100,
+            color: '#F58A00'
+        };
+        vm.logOut = function () {
+            authService.logOut();
+            $location.path('/home');
+        }
+
+        vm.signUp = function () {
+            $location.path('/signup');
+        }
+
+        
+
+        vm.authentication = authService.authentication;
+
+        activate();
+
+        function activate() {
+            logSuccess('Med Web loaded!', null, true);
+            common.activateController([], controllerId);
+            $location.path('/home');
+        }
+
+        function toggleSpinner(on) { vm.isBusy = on; }
+
+        $rootScope.$on('$routeChangeStart',
+            function (event, next, current) { toggleSpinner(true); }
+        );
+        
+        $rootScope.$on(events.controllerActivateSuccess,
+            function (data) { toggleSpinner(false); }
+        );
+
+        $rootScope.$on(events.spinnerToggle,
+            function (data) { toggleSpinner(data.show); }
+        );
+    };
+})();
