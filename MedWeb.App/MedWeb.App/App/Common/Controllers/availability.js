ab846bf86f4f15fb27618a88fdf74426dd25592a
sourav-gantait <sourav_gantait@yahoo.co.in>
2016-01-17 21:53:51 +0530

    change on availability js

--- MedWeb.App/MedWeb.App/App/Common/Controllers/availability.js
+++ MedWeb.App/MedWeb.App/App/Common/Controllers/availability.js
@@ -1,8 +1,9 @@
 ﻿
-var app = angular.module("app", []);
+var app = angular.module("app");
 app.controller("available", function ($scope) {
     
-    $scope.locations = [{ name: 'Dum Dum', code:'dd' },
+    $scope.locations = [
+        { name: 'Dum Dum', code: 'dd' },
         { name: 'Garia',code:'ga' },
         { name: 'Jadavpur',code:'jd' }];
 });
