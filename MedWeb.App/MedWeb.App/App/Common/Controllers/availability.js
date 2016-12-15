9565e71e5d766f8fa4fab43004a89fbba02f3389
SOURAV <SOURAV@LAPTOP-KGDAHJ01>
2016-12-15 19:27:24 +0530

    change on base

--- MedWeb.App/MedWeb.App/App/Common/Controllers/availability.js
+++
@@ -1,8 +0,0 @@
-﻿
-var app = angular.module("app", []);
-app.controller("available", function ($scope) {
-    
-    $scope.locations = [{ name: 'Dum Dum', code:'dd' },
-        { name: 'Garia',code:'ga' },
-        { name: 'Jadavpur',code:'jd' }];
-});
